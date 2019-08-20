d7618932491921a2d6fb92ee9730cd793ba6f205
Samuel Webb <samuel.webb@cern.ch>
2019-08-20 13:43:17 +0100

    clean up Plotter.cxx

--- src/Plotter.cxx
+++ src/Plotter.cxx
@@ -282,35 +282,6 @@ TGraphErrors * Plotter::DrawProfile(TH2F * hist, TString savename, Option_t * op
   TProfile* pmean = hist->ProfileX( "profile_mean");
   TProfile* p_rms = hist->ProfileX( "profile", 1, -1, option );
 
-  // TF1 * f_mean = new TF1 ( "f_mean", "[0] + [1]*x" , 0, 400);
-  // TF1 * f_rmsup = new TF1 ( "f_rmsup", "[0] + [1]*x" ,0 , 400);
-  // TF1 * f_rmsdown = new TF1 ( "f_rmsdown", "[0] + [1]*x",0, 400 );
-
-
-  // TF1 * f_mean = new TF1 ( "f_mean", "[0] + [1]*x + [2]*x*x" , 0, 400);
-  // TF1 * f_rmsup = new TF1 ( "f_rmsup", "[0] + [1]*x + [2]*x*x" ,0 , 400);
-  // TF1 * f_rmsdown = new TF1 ( "f_rmsdown", "[0] + [1]*x + [2]*x*x",0, 400 );
-
-  //  TF1 * Logx = new TF1 ( "logx", "[0]+ [1]*TMath::Log(x+[2]) + [3]*TMath::Log(x+[2])*TMath::Log(x+[2])*x" );
-  //  TF1 * Logx = new TF1 ( "logx", "[0]+ [1]*TMath::Log(x+[2]) + [3]*pow(TMath::Log(x+[4]),2)*x" );
-  //  TF1 * Logx = new TF1 ( "logx", "[0]+ [1]*pow(TMath::Log(x),2)*x + [2]*TMath::Log(x)" );
-  ///    TF1 * Logx = new TF1 ( "logx", "[0]+ [1]*pow(TMath::Log(x),2)*x + [2]*TMath::Log(x+)" );
-  //    TF1 * Logx = new TF1 ( "logx", "[0]+ [1]*pow(TMath::Log(x),2)*TMath::Exp(-x) + [2]*x" );
-
-  //    TF1 * Logx = new TF1 ( "logx", "[0]+ [1]*pow(TMath::Log([2]*x),2)*x - TMath::Log([2]*x+20)" );
-
-  //    TF1 * Logx = new TF1 ( "logx", "[0] + [1]*pow(TMath::Log(10*x),2)*x -[2]*TMath::Log(10*x+50)" );
-
-  //    TF1 * Logx = new TF1 ( "logx", "[0] + [1]*pow(TMath::Log([2]*1000000000*x),2)*x -[3]*TMath::Log(10*x+200)" );
-  //    TF1 * Logx = new TF1 ( "logx", "[0] + [1]*pow(TMath::Log([2]*100000*x),1)*x -[3]*TMath::Log(10*x+200)" );
-
-
-  TF1 * Logx = new TF1 ( "logx", "[0] + [1]*TMath::Log(x)*x + [2]*x -[3]*TMath::Log([4]*x+200)" , 5 , 350);  
-  Logx->SetParLimits( 0, 10,1000);
-  Logx->SetParLimits( 1, -0.5,0.5);
-  Logx->SetParLimits( 2, 0.001,10);
-  Logx->SetParLimits( 3, 1,1000);
-  Logx->SetParLimits( 4, 0.001,50);
   double start = 20;
   double end = 300;
 
@@ -323,59 +294,26 @@ TGraphErrors * Plotter::DrawProfile(TH2F * hist, TString savename, Option_t * op
   else if (  order ==2){
     pol_choice  = new TF1 ( "pol", "[0] + [1]*x + [2]*x*x" , 20 , 350);
   }
-  //TF1 * pol1 = new TF1 ( "pol", "[0] + [1]*x" , 20 , 350);  
-  //    TF1 * pol2 = new TF1 ( "pol", "[0] + [1]*x + [2]*x*x" , 20 , 350);  
-
-    //    Logx->SetParLimits( 5, 100,300);
-
-  // tf1 * Logx = new TF1 ( "logx", "[0]+ [1]*pow(TMath::Log(x+[3]),2)*x + [2]*TMath::Log(x+[4])" );
-
-  // TF1 * f_mean = new TF1 ( "f_mean", "[0] + [1]*x + [2]*x*x + [3]*x*x*x" , 0, 400);
-  // TF1 * f_rmsup = new TF1 ( "f_rmsup", "[0] + [1]*x + [2]*x*x + [3]*x*x*x" ,0 , 400);
-  // TF1 * f_rmsdown = new TF1 ( "f_rmsdown", "[0] + [1]*x + [2]*x*x + [3]*x*x*x",0, 400 );
-
-  // TF1 * f_mean = new TF1 ( "f_mean", Logx , 5, 350);
-  // TF1 * f_rmsup = new TF1 ( "f_rmsup", Logx , 5, 350);
-  // TF1 * f_rmsdown = new TF1 ( "f_rmsdown", Logx , 5, 350);
-
-
 
 
   TF1 * f_mean = (TF1*)pol_choice->Clone("f_mean" );
   TF1 * f_rmsup = (TF1*)pol_choice->Clone("f_rmsup" );
   TF1 * f_rmsdown = (TF1*)pol_choice->Clone("f_rmsdown" );
 
-  // TF1 * f_mean = (TF1*)Logx->Clone("f_mean" );
-  // TF1 * f_rmsup = (TF1*)Logx->Clone("f_rmsup" );
-  // TF1 * f_rmsdown = (TF1*)Logx->Clone("f_rmsdown" );
-
   TFitResultPtr r;
   TFitResultPtr r_rmsup;
   TFitResultPtr r_rmsdown;
 
-   //   r =  pmean->Fit( "pol1", "R","",20,200);
-   //   r =  pmean->Fit( "pol3", "R","",0,400);
-
-   //   r =  pmean->Fit( "[0]+[1]*TMath::Log(x+[2])*x", "R","",0,400);
-   //   r =  pmean->Fit( "[0]*TMath::Erf(x)", "R","",0,400);
-   //   TVirtualFitter::SetMaxIterations( 1000000 );
-
-   //   r =  pmean->Fit( Logx, "R","",5,350);
-
-
-   r =  pmean->Fit( pol_choice, "R","",start,end);
-
-
-
-   pmean->Draw();
-   pmean->GetYaxis()->SetRangeUser(0,400);
-   pmean->Draw("same");
-
-   p_rms->Draw("same");
-
+  r =  pmean->Fit( pol_choice, "R","",start,end);
+  
+  pmean->Draw();
+  pmean->GetYaxis()->SetRangeUser(0,400);
+  pmean->Draw("same");
+  
+  p_rms->Draw("same");
+  
   TGraphErrors * rms_up = new TGraphErrors( p_rms->GetNbinsX() );
   TGraphErrors * rms_down = new TGraphErrors( p_rms->GetNbinsX() );
-
   TGraphErrors * resolution = new TGraphErrors( p_rms->GetNbinsX() );
   resolution->SetTitle( "" );
 
@@ -387,11 +325,7 @@ TGraphErrors * Plotter::DrawProfile(TH2F * hist, TString savename, Option_t * op
   bool bootstrap = false;
  
   for (int i = 1; i < p_rms->GetNbinsX()+1; i++){
-    //    std::cout << "we have " <<  p_rms->GetNbinsX()+1 << " bins " << std::endl;
-    //    std::cout << "bin i of x = " << i << std::endl;
-
     std::vector<double> rms_list;  
-    //    std::vector<double> rms_err_list;
     int centralbin_nominal = 0;
     double xval_nom = p_rms->GetBinContent(i);
     for (int y = 1; y < hist->GetNbinsY()+1; y++){
@@ -433,12 +367,6 @@ TGraphErrors * Plotter::DrawProfile(TH2F * hist, TString savename, Option_t * op
 	else{
 	  rms_list.push_back(100);      
 	}
-
-	// rms_up  ->SetPoint(i-1, p_rms->GetBinCenter(i), p_rms->GetBinContent(i)+p_rms->GetBinError(i));
-	// rms_down->SetPoint(i-1, p_rms->GetBinCenter(i), p_rms->GetBinContent(i)-p_rms->GetBinError(i));
-	
-      // rms_up  ->SetPointError(i-1, p_rms->GetBinWidth(i)/2, pmean->GetBinError(i) );
-      // rms_down->SetPointError(i-1, p_rms->GetBinWidth(i)/2, pmean->GetBinError(i) );
 	
 	histCopy->Delete();
 
@@ -447,13 +375,11 @@ TGraphErrors * Plotter::DrawProfile(TH2F * hist, TString savename, Option_t * op
     
       auto min_value = *std::min_element(rms_list.begin(),rms_list.end());
       auto max_value = *std::max_element(rms_list.begin(),rms_list.end());
-      //      std::cout << "min max -= " << min_value << " - " << max_value << std::endl;
+
       TH1F* rms_histo=new TH1F("rms_histo","rms_histo",2000,min_value-1,max_value+1);
       for(unsigned int a=0; a<rms_list.size(); a++){
 	rms_histo->Fill(rms_list.at(a));
       }
-
-
       
       rms.push_back( rms_histo->GetMean() );
       rms_err.push_back( rms_histo->GetStdDev() );
@@ -464,7 +390,7 @@ TGraphErrors * Plotter::DrawProfile(TH2F * hist, TString savename, Option_t * op
       
       double err = 0;
       double total = hist->IntegralAndError(i,i,1,hist->GetNbinsY() , err);
-      //      std::cout << "Total = " << total << std::endl;
+
       if (total>0.00001){ 
 	
 	for (int y = 1; y < hist->GetNbinsY()+1; y++){
@@ -492,48 +418,23 @@ TGraphErrors * Plotter::DrawProfile(TH2F * hist, TString savename, Option_t * op
     }
 
 
-    
-    //    std::cout << rms.size() << " - " << rms_err.size() << std::endl;
-
 
     //normal way    
     rms_up  ->SetPoint(i-1, p_rms->GetBinCenter(i), double(centralbin_nominal+rms.at(i-1))/double(bins_per_unit) );
     rms_down->SetPoint(i-1, p_rms->GetBinCenter(i), double(centralbin_nominal-rms.at(i-1))/double(bins_per_unit) );
     
-    //test way
-    // rms_up  ->SetPoint(i-1, p_rms->GetBinCenter(i), p_rms->GetBinContent(i)+p_rms->GetBinError(i) );
-    // rms_down  ->SetPoint(i-1, p_rms->GetBinCenter(i), p_rms->GetBinContent(i)-p_rms->GetBinError(i) );
-    
-
-
-
-
-
-
-    //    std::cout << " ------- " << double (rms_err.at(i-1)) << " - " << double (rms_err.at(i-1))/double (bins_per_unit) << std::endl;
-
     rms_up  ->SetPointError(i-1, 0, double (rms_err.at(i-1))/double (bins_per_unit) );
     rms_down->SetPointError(i-1, 0, double (rms_err.at(i-1))/double (bins_per_unit) );
-    // rms_up  ->SetPointError(i-1, 0.5*p_rms->GetBinWidth(i), double (rms_err.at(i-1))/double (bins_per_unit) );
-    // rms_down->SetPointError(i-1, 0.5*p_rms->GetBinWidth(i), double (rms_err.at(i-1))/double (bins_per_unit) );
      
   }
 
 
-  
-  // r = rms_up->Fit("pol3", "R", "", 0, 400);
-  // r = rms_down->Fit("pol3", "R", "", 0, 400);
-
-
-
   r_rmsup = rms_up->Fit(pol_choice, "RS", "", start, end);
-  
   r = rms_down->Fit(pol_choice, "R", "", start, end);  
 
   f_mean->SetParameter( 0, pmean->GetFunction("pol")->GetParameter( 0 ) ); 
   f_mean->SetParameter( 1, pmean->GetFunction("pol")->GetParameter( 1 ) );
   f_mean->SetParameter( 2, pmean->GetFunction("pol")->GetParameter( 2 ) );
-  
   f_mean->SetParErrors ( pmean->GetFunction("pol")->GetParErrors() );
   
   f_rmsup->SetParameter( 0, rms_up->GetFunction("pol")->GetParameter( 0 ) );
@@ -545,23 +446,17 @@ TGraphErrors * Plotter::DrawProfile(TH2F * hist, TString savename, Option_t * op
   f_rmsdown->SetParameter( 1, rms_down->GetFunction("pol")->GetParameter( 1 ) );
   f_rmsdown->SetParameter( 2, rms_down->GetFunction("pol")->GetParameter( 2 ) );
   f_rmsdown->SetParErrors ( rms_down->GetFunction("pol")->GetParErrors() );
-
-  
+ 
   f_rmsup->SetRange(start,end);
   f_rmsdown->SetRange(start,end);
 
   f_rmsup->Draw("same");
   f_rmsdown->Draw("same");
-  f_mean->SetLineColor(kGreen);//temp remove
-  f_mean->Draw("same");//temp remove
-
-
-  //rms_up->Draw("same");
-  //rms_down->Draw("same");
+  f_mean->SetLineColor(kGreen);
+  f_mean->Draw("same");
 
   pmean->Draw("same");		
   gPad->SetTicks();
-  //  gPad->RedrawAxis();
 
   c->SaveAs("plots/" + TString(_outdir) + "/" + savename + ".png");
   c->SaveAs("plots/" + TString(_outdir) + "/" + savename + ".root");
@@ -570,7 +465,6 @@ TGraphErrors * Plotter::DrawProfile(TH2F * hist, TString savename, Option_t * op
 
   std::cout << "--------------" << std::endl;
   for (int i = 1; i < p_rms->GetNbinsX()+1; i++){
-
 
     //Using the fits
     double x[1] = { p_rms->GetBinCenter(i) };
@@ -607,7 +501,6 @@ TGraphErrors * Plotter::DrawProfile(TH2F * hist, TString savename, Option_t * op
 
     resolution->SetPoint(i-1, p_rms->GetBinCenter(i), sigma/p_rms->GetBinCenter(i));
     resolution->SetPointError(i-1, 0.5*p_rms->GetBinWidth(i), std::abs(sigma-sigma_err)/p_rms->GetBinCenter(i));
-
 
   }
 
